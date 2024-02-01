7d23b5fb3df4602d5dbea35d77965cb24f25ea2c
Chung-Chiang Cheng <cccheng@synology.com>
2024-02-01 09:47:11 +0800

    nvim: add nvim-scrollbar
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -16,4 +16,20 @@ return {
             require('lualine').setup()
         end
     },
+    {
+        "petertriho/nvim-scrollbar",
+        config = function()
+            require("scrollbar").setup({
+                hide_if_all_visible = true, -- Hides everything if all lines are visible
+                handlers = {
+                    cursor = true,
+                    diagnostic = true,
+                    gitsigns = true, -- Requires gitsigns
+                    handle = true,
+                    search = false, -- Requires hlslens
+                    ale = false, -- Requires ALE
+                },
+            })
+        end
+    },
 }
