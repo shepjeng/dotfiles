a609a6f0c538b579e31a336e21a1bbefba2120fe
Chung-Chiang Cheng <cccheng@synology.com>
2024-02-04 14:25:49 +0800

    nvim: update telescope
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -29,19 +29,19 @@ return {
                 pickers = {
                     find_files = {
                         theme = "ivy",
-                        winblend = 10,
+                        winblend = 17,
                     },
                     live_grep = {
                         theme = "ivy",
-                        winblend = 10,
+                        winblend = 17,
                     },
                     buffers = {
                         theme = "ivy",
-                        winblend = 10,
+                        winblend = 17,
                     },
                     help_tags = {
                         theme = "ivy",
-                        winblend = 10,
+                        winblend = 17,
                     },
                 },
                 extensions = {
@@ -73,28 +73,13 @@ return {
             require("telescope").setup({
                 extensions = {
                     ["ui-select"] = {
-                        require("telescope.themes").get_dropdown {
-                            -- even more opts
-                        }
-
-                        -- pseudo code / specification for writing custom displays, like the one
-                        -- for "codeactions"
-                        -- specific_opts = {
-                        --   [kind] = {
-                        --     make_indexed = function(items) -> indexed_items, width,
-                        --     make_displayer = function(widths) -> displayer
-                        --     make_display = function(displayer) -> function(e)
-                        --     make_ordinal = function(e) -> string
-                        --   },
-                        --   -- for example to disable the custom builtin "codeactions" display
-                        --      do the following
-                        --   codeactions = false,
-                        -- }
+                        require("telescope.themes").get_dropdown({
+                            winblend = 17,
+                        })
                     }
                 }
             })
-            -- To get ui-select loaded and working with telescope, you need to call
-            -- load_extension, somewhere after setup function:
+
             require("telescope").load_extension("ui-select")
         end
     },
@@ -110,6 +95,7 @@ return {
                 extensions = {
                     file_browser = {
                         theme = "ivy",
+                        winblend = 17,
                         -- disables netrw and use telescope-file-browser in its place
                         hijack_netrw = true,
                     },
