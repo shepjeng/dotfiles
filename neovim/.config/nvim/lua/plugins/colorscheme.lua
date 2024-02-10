bdfcbca29af7498f633731d6d0e519283be10328
Chung-Chiang Cheng <cccheng@synology.com>
2024-02-10 21:02:20 +0800

    nvim: update colorscheme
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,43 @@
 return {
     {
-        "folke/tokyonight.nvim",
+        "catppuccin/nvim",
         event = "VeryLazy",
+        config = function()
+            require("catppuccin").setup({
+                integrations = {
+                    cmp = true,
+                    gitsigns = true,
+                    harpoon = true,
+                    illuminate = true,
+                    indent_blankline = {
+                        enabled = false,
+                        scope_color = "sapphire",
+                        colored_indent_levels = false,
+                    },
+                    mason = true,
+                    native_lsp = { enabled = true },
+                    notify = true,
+                    nvimtree = true,
+                    neotree = true,
+                    symbols_outline = true,
+                    telescope = true,
+                    treesitter = true,
+                    treesitter_context = true,
+                },
+            })
+
+            -- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
+            for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+                vim.api.nvim_set_hl(0, group, {})
+            end
+        end,
     }, {
-        "cocopon/iceberg.vim",
+        -- "cocopon/iceberg.vim",
+        "oahlen/iceberg.nvim",
         lazy = false,
         priority = 1000,
         config = function()
-            vim.cmd.colorscheme "iceberg"
+            vim.cmd.colorscheme("iceberg")
         end
     }
 }
