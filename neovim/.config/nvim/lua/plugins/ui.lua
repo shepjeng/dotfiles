90ac62a854eb700ce68c5dd1b9fe9eebdfe23127
Chung-Chiang Cheng <cccheng@synology.com>
2024-02-17 18:04:08 +0800

    nvim: add leap
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -35,11 +35,19 @@ return {
     },
     {
         "jinh0/eyeliner.nvim",
+        event = "VeryLazy",
         config = function()
             require("eyeliner").setup({
                 highlight_on_key = true, -- show highlights only after keypress
                 dim = false, -- dim all other characters if set to true (recommended!)
             })
+        end
+    },
+    {
+        "ggandor/leap.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("leap").create_default_mappings()
         end
     },
     {
