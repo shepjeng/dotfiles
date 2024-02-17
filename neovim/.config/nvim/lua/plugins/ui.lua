0040a932b929729fab19a7ce0de01500581ed01f
Chung-Chiang Cheng <cccheng@synology.com>
2024-02-17 17:51:14 +0800

    nvim: add eyeliner
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -34,6 +34,15 @@ return {
         end
     },
     {
+        "jinh0/eyeliner.nvim",
+        config = function()
+            require("eyeliner").setup({
+                highlight_on_key = true, -- show highlights only after keypress
+                dim = false, -- dim all other characters if set to true (recommended!)
+            })
+        end
+    },
+    {
         "echasnovski/mini.nvim",
         event = "VeryLazy",
         config = function()
