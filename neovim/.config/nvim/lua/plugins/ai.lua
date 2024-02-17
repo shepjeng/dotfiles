69aa58c0e472db2b8ffe2e3b235c1ed146e8c37f
Chung-Chiang Cheng <cccheng@synology.com>
2024-02-17 23:38:22 +0800

    nvim: add custom actions to ogpt
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/ai.lua
+++ neovim/.config/nvim/lua/plugins/ai.lua
@@ -13,7 +13,13 @@ return {
             },
             {
                 "<leader>ga", "<cmd>OGPTActAs<cr>", desc = "GPT act as",
-            }
+            },
+            {
+                "<leader>gg", "<cmd>OGPTRun grammar_correction<cr>", desc = "GPT Grammar Correction", mode = { "v" },
+            },
+            {
+                "<leader>gt", "<cmd>OGPTRun translate<cr>", desc = "GPT Translate", mode = { "v" },
+            },
         },
         opts = {
             default_provider = "gemini",
@@ -26,7 +32,7 @@ return {
             },
             popup = {
                 border = {
-                    style = "none",
+                    style = "rounded",
                 },
             },
             popup_layout = {
@@ -40,6 +46,25 @@ return {
                 border = {
                     text = {
                         top = "",
+                    },
+                },
+            },
+            actions = {
+                grammar_correction = {
+                    type = "popup",
+                    template = "Correct the given text to standard English:\n\n```{{input}}```",
+                    system = "You are a helpful note writing assistant, given a text input, correct the text only for grammar and spelling error. You are to keep all formatting the same, e.g. markdown bullets, should stay as a markdown bullet in the result, and indents should stay the same. Return ONLY the corrected text.",
+                    strategy = "display",
+                    params = {
+                        temperature = 0.3,
+                    },
+                },
+                translate = {
+                    type = "popup",
+                    template = "Translate this into Traditional Chinese:\n\n{{input}}",
+                    strategy = "display",
+                    params = {
+                        temperature = 0.3,
                     },
                 },
             },
