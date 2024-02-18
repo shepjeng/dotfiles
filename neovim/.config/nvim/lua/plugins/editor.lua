580c5a3ac8d6ec0cc75fc0718db0bbd993731eeb
Chung-Chiang Cheng <cccheng@synology.com>
2024-02-18 19:54:09 +0800

    nvim: add flash to replace eyelinear/leap
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/editor.lua
+++ neovim/.config/nvim/lua/plugins/editor.lua
@@ -17,20 +17,15 @@ return {
         end
     },
     {
-        "jinh0/eyeliner.nvim",
+        "folke/flash.nvim",
         event = "VeryLazy",
-        config = function()
-            require("eyeliner").setup({
-                highlight_on_key = true, -- show highlights only after keypress
-                dim = false, -- dim all other characters if set to true (recommended!)
-            })
-        end
-    },
-    {
-        "ggandor/leap.nvim",
-        event = "VeryLazy",
-        config = function()
-            require("leap").create_default_mappings()
-        end
+        keys = {
+            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+            { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+            -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        },
+        opts = {},
     },
 }
