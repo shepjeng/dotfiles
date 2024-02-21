97af28765f6bbca715ec00f4f978b56fea09e7d7
Chung-Chiang Cheng <cccheng@synology.com>
2024-02-21 12:26:03 +0800

    nvim: add tundra
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/theme.lua
+++ neovim/.config/nvim/lua/plugins/theme.lua
@@ -8,4 +8,13 @@ return {
             vim.cmd.colorscheme("iceberg")
         end
     },
+    {
+        "sam4llis/nvim-tundra",
+        event = "VeryLazy",
+        config = function()
+            require("nvim-tundra").setup({
+                transparent_background = true,
+            })
+        end
+    },
 }
