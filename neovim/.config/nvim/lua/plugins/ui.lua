701c5dc33e5db12610498b7d47f2f9e3330ae4b3
Chung-Chiang Cheng <cccheng@synology.com>
2024-02-23 16:08:10 +0800

    nvim: update lualine filename
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -15,7 +15,21 @@ return {
             "nvim-tree/nvim-web-devicons",
         },
         config = function()
-            require('lualine').setup()
+            require('lualine').setup({
+                sections = {
+                    lualine_a = {'mode'},
+                    lualine_b = {'branch', 'diff', 'diagnostics'},
+                    lualine_c = {
+                        {
+                            'filename',
+                            path = 3,
+                        }
+                    },
+                    lualine_x = {'encoding', 'fileformat', 'filetype'},
+                    lualine_y = {'progress'},
+                    lualine_z = {'location'}
+                },
+            })
         end
     },
     {
