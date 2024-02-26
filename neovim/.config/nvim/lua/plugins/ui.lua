9460e19c6a25b621865339a914a2994837bb3b1b
Chung-Chiang Cheng <cccheng@synology.com>
2024-02-26 17:20:00 +0800

    nvim: show recording
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -18,7 +18,14 @@ return {
             require('lualine').setup({
                 sections = {
                     lualine_a = {'mode'},
-                    lualine_b = {'branch', 'diff', 'diagnostics'},
+                    lualine_b = {
+                        'branch', 'diff', 'diagnostics',
+                        {
+                            require("noice").api.statusline.mode.get,
+                            cond = require("noice").api.statusline.mode.has,
+                            color = { fg = "#ff9e64" },
+                        },
+                    },
                     lualine_c = {
                         {
                             'filename',
@@ -96,6 +103,12 @@ return {
                 },
                 messages = {
                     view_search = false,
+                },
+                routes = {
+                    {
+                        view = "notify",
+                        filter = { event = "msg_showmode" },
+                    },
                 },
             })
 
