c4cd12a4a09dcdf05e8a18f1fd473091ab81e9e9
Chung-Chiang Cheng <cccheng@synology.com>
2024-03-04 21:20:02 +0800

    nvim: update ogpt
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/ai.lua
+++ neovim/.config/nvim/lua/plugins/ai.lua
@@ -30,18 +30,6 @@ return {
                     },
                 },
             },
-            popup = {
-                border = {
-                    style = "rounded",
-                },
-            },
-            popup_layout = {
-                default = "right",
-                right = {
-                    width = "45%",
-                    width_parameters_open = "45%",
-                },
-            },
             output_window = {
                 border = {
                     text = {
@@ -52,19 +40,33 @@ return {
             actions = {
                 grammar_correction = {
                     type = "popup",
-                    template = "Correct the given text to standard English:\n\n```{{input}}```",
+                    template = "Correct the given text to standard English:\n\n```{{{input}}}```",
                     system = "You are a helpful note writing assistant, given a text input, correct the text only for grammar and spelling error. You are to keep all formatting the same, e.g. markdown bullets, should stay as a markdown bullet in the result, and indents should stay the same. Return ONLY the corrected text.",
-                    strategy = "display",
+                    strategy = "display", -- replace
                     params = {
                         temperature = 0.3,
+                    },
+                    args = {
+                        lang = {
+                            type = "string",
+                            optional = "true",
+                            default = "english",
+                        },
                     },
                 },
                 translate = {
                     type = "popup",
-                    template = "Translate this into Traditional Chinese:\n\n{{input}}",
+                    template = "Translate this into Traditional Chinese:\n\n{{{input}}}",
                     strategy = "display",
                     params = {
                         temperature = 0.3,
+                    },
+                    args = {
+                        lang = {
+                            type = "string",
+                            optional = "true",
+                            default = "chinese",
+                        },
                     },
                 },
             },
