42379298521acb46da0d36fb34bd45523c1950ce
Chung-Chiang Cheng <cccheng@synology.com>
2024-03-04 09:40:34 +0800

    nvim: remove translate
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/translate.lua
+++
@@ -1,9 +0,0 @@
-return {
-    {
-        "uga-rosa/translate.nvim",
-        event = "BufReadPre",
-        config = function()
-            vim.keymap.set("n", "<leader>gt", "<cmd>Translate ZH-TW<cr>", { desc = "Translate" })
-        end,
-    },
-}
