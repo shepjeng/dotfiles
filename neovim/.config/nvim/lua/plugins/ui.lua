af677de4fe74b63eda2bb584f7f993bbe5a41d5e
Chung-Chiang Cheng <cccheng@synology.com>
2024-03-28 11:28:03 +0800

    nvim: update indent-blankline
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -5,6 +5,10 @@ return {
         config = function()
             require("ibl").setup({
                 indent = { char = "▏" },
+                scope = {
+                    show_start = false,
+                    show_end = false,
+                },
             })
         end
     },
