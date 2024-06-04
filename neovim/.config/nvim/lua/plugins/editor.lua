16daacb1dfba029e027cab1e75ba1fd5b6274dbe
Chung-Chiang Cheng <cccheng@synology.com>
2024-06-04 14:34:12 +0800

    nvim: set up nvim-bqf
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/editor.lua
+++ neovim/.config/nvim/lua/plugins/editor.lua
@@ -38,5 +38,17 @@ return {
     {
         "kevinhwang91/nvim-bqf",
         ft = { "qf" },
+        config = function()
+            require("bqf").setup({
+                auto_enable = true,
+                auto_resize_height = true,
+                preview = {
+                    win_height = 24,
+                    border = {'┏', '━', '┓', '┃', '┛', '━', '┗', '┃'},
+                    show_title = false,
+                    winblend = 10,
+                }
+            })
+        end,
     },
 }
