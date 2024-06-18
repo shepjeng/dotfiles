87fba4d95ed18fef1ab2960db8a8a9a4858ee933
Chung-Chiang Cheng <cccheng@synology.com>
2024-06-18 14:49:31 +0800

    nvim: update tabline color of iceberg
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/theme.lua
+++ neovim/.config/nvim/lua/plugins/theme.lua
@@ -8,8 +8,8 @@ return {
             vim.cmd.colorscheme("iceberg")
 
             vim.api.nvim_set_hl(0, "TabLine", { fg = "#9a9ca5", bg = "#1f2233", bold = false })
-            vim.api.nvim_set_hl(0, "TabLineFill", { fg = "#101218", bg = "#3f455f", bold = false })
-            vim.api.nvim_set_hl(0, "TabLineSel", { fg = "#9a9ca5", bg = "#1f2233", bold = true })
+            vim.api.nvim_set_hl(0, "TabLineFill", { fg = "#101218", bg = "#1f2233", bold = false })
+            vim.api.nvim_set_hl(0, "TabLineSel", { fg = "#9a9ca5", bg = "#2b335a", bold = true })
         end
     },
     {
