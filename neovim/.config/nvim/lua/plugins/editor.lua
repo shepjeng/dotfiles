fed41d993e33274f7b1726d15d33cef2bd5f07a0
Chung-Chiang Cheng <cccheng@synology.com>
2024-08-14 11:49:43 +0800

    nvim: Add autoclose.nvim
    - Add autoclose.nvim to the plugin list
    - Configure autoclose.nvim to be loaded on BufReadPre event
    
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/editor.lua
+++ neovim/.config/nvim/lua/plugins/editor.lua
@@ -68,4 +68,12 @@ return {
             })
         end,
     },
+    {
+        "m4xshen/autoclose.nvim",
+        event = "BufReadPre",
+        config = function()
+            require("autoclose").setup({
+            })
+        end,
+    }
 }
