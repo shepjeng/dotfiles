41cb007ac6106573726f8fbc1e24d44f001c8c79
Chung-Chiang Cheng <cccheng@synology.com>
2024-08-15 23:12:19 +0800

    feat(nvim): remove unused autocmd group
    Remove the unused autocmd group `YankHighlight`.
    
    This change simplifies the configuration and improves readability.
    
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/config/autocmds.lua
+++ neovim/.config/nvim/lua/config/autocmds.lua
@@ -1,10 +1,7 @@
 
 -- highlight on yank
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
     callback = function()
         vim.highlight.on_yank()
     end,
-    group = highlight_group,
-    pattern = "*",
 })
