24aefb02c7433e621fc69db3ba6f540e0aa0316f
Chung-Chiang Cheng <cccheng@synology.com>
2024-08-19 17:20:00 +0800

    feat(nvim): update autocmds to show cursor line only in active window
        - Add autocmds to show cursor line only in the active window.
        - Show cursor line when entering normal mode or a new window,
          and hide it when entering insert mode or leaving a window.
        - This improves the visual clarity of the active window.
        - This also improves the visual clarity of the active window
          when switching between buffers.
        - Also moved the relative line number autocmds to a separate
          autocmd group.
        - This makes it easier to manage and understand the different
          autocmds.
        - This also improves the visual clarity of the active window
          when switching between buffers.
    
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/config/autocmds.lua
+++ neovim/.config/nvim/lua/config/autocmds.lua
@@ -6,34 +6,49 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     end,
 })
 
+-- Show cursor line only in active window
+vim.api.nvim_create_autocmd({ 'InsertLeave', 'WinEnter' }, {
+    callback = function(event)
+        if vim.bo[event.buf].buftype == '' then
+            vim.opt_local.cursorline = true
+        end
+    end,
+})
+vim.api.nvim_create_autocmd({ 'InsertEnter', 'WinLeave' }, {
+    callback = function()
+        vim.opt_local.cursorline = false
+    end,
+})
+
+
 -- relative line numbers in normal mode
 -- absolute line numbers in insert/command mode
 local numbertoggle = vim.api.nvim_create_augroup("numbertoggle", {})
 vim.api.nvim_create_autocmd(
-    {
-        "BufEnter", "FocusGained", "InsertLeave", "WinEnter", "CmdlineLeave"
-    },
-    {
-        group = numbertoggle,
-        callback = function()
-            if vim.opt.number and vim.api.nvim_get_mode() ~= "i" then
-                vim.opt.relativenumber = true
-            end
-        end,
-    }
+{
+    "BufEnter", "FocusGained", "InsertLeave", "WinEnter", "CmdlineLeave"
+},
+{
+    group = numbertoggle,
+    callback = function()
+        if vim.opt.number and vim.api.nvim_get_mode() ~= "i" then
+            vim.opt.relativenumber = true
+        end
+    end,
+}
 )
 
 vim.api.nvim_create_autocmd(
-    {
-        "BufLeave", "FocusLost", "InsertEnter", "WinLeave", "CmdlineEnter"
-    },
-    {
-        group = numbertoggle,
-        callback = function()
-            if vim.opt.number then
-                vim.opt.relativenumber = false
-                vim.cmd("redraw")
-            end
-        end,
-    }
+{
+    "BufLeave", "FocusLost", "InsertEnter", "WinLeave", "CmdlineEnter"
+},
+{
+    group = numbertoggle,
+    callback = function()
+        if vim.opt.number then
+            vim.opt.relativenumber = false
+            vim.cmd("redraw")
+        end
+    end,
+}
 )
