fb7f99cdb31c8e88d19344a63654c026ccf67cd5
Chung-Chiang Cheng <cccheng@synology.com>
2024-08-26 15:58:09 +0800

    chore(nvim): remove autocmd for setting relative line numbers
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/config/autocmds.lua
+++ neovim/.config/nvim/lua/config/autocmds.lua
@@ -20,34 +20,3 @@ vim.api.nvim_create_autocmd({ 'InsertEnter', 'WinLeave' }, {
     end,
 })
 
--- relative line numbers in normal mode
--- absolute line numbers in insert/command mode
-local numbertoggle = vim.api.nvim_create_augroup("numbertoggle", {})
-vim.api.nvim_create_autocmd(
-{
-    "BufEnter", "FocusGained", "InsertLeave", "WinEnter", "CmdlineLeave"
-},
-{
-    group = numbertoggle,
-    callback = function()
-        if vim.opt.number and vim.api.nvim_get_mode() ~= "i" then
-            vim.opt.relativenumber = true
-        end
-    end,
-}
-)
-
-vim.api.nvim_create_autocmd(
-{
-    "BufLeave", "FocusLost", "InsertEnter", "WinLeave", "CmdlineEnter"
-},
-{
-    group = numbertoggle,
-    callback = function()
-        if vim.opt.number then
-            vim.opt.relativenumber = false
-            vim.cmd("redraw")
-        end
-    end,
-}
-)
