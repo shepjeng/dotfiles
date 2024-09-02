428834377060f36a8ed6de379deab3aa1c6604da
Chung-Chiang Cheng <cccheng@synology.com>
2024-09-02 09:51:54 +0800

    feat(nvim): Add toggleterm plugin
    - Add toggleterm plugin
    - Update autocmd config to use lua tables
    - Fix autocmd config to use empty string for `buftype` check
    
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/config/autocmds.lua
+++ neovim/.config/nvim/lua/config/autocmds.lua
@@ -7,14 +7,14 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- Show cursor line only in active window
-vim.api.nvim_create_autocmd({ 'InsertLeave', 'WinEnter' }, {
+vim.api.nvim_create_autocmd({"InsertLeave", "WinEnter"}, {
     callback = function(event)
-        if vim.bo[event.buf].buftype == '' then
+        if vim.bo[event.buf].buftype == "" then
             vim.opt_local.cursorline = true
         end
     end,
 })
-vim.api.nvim_create_autocmd({ 'InsertEnter', 'WinLeave' }, {
+vim.api.nvim_create_autocmd({"InsertEnter", "WinLeave"}, {
     callback = function()
         vim.opt_local.cursorline = false
     end,
---
+++ neovim/.config/nvim/lua/plugins/terminal.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "akinsho/toggleterm.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("toggleterm").setup({
+                size = 17,
+                open_mapping = "<LEADER>`",
+            })
+        end
+    }
+}
