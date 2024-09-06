c9ea8fdf181a7acb6061593164238ebe059c46b7
Chung-Chiang Cheng <cccheng@synology.com>
2024-09-06 17:57:41 +0800

    chore(nvim): Configure render-markdown
      mini.nvim
    - - Add code block options to render-markdown
    - - Add heading block options to render-markdown
    
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/markdown.lua
+++ neovim/.config/nvim/lua/plugins/markdown.lua
@@ -2,11 +2,19 @@ return {
     {
         "MeanderingProgrammer/render-markdown.nvim",
         ft = "markdown",
-        -- name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
         -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
         -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
         dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
         opts = {
+            heading = {
+                sign = false,
+                icons = {},
+            },
+            code = {
+                sign = false,
+                language_pad = 1,
+                left_pad = 1,
+            },
         },
     }
 }
