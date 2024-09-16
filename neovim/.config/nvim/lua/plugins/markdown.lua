a5957b6043a33a0cfbc90fbc88bb4c5be4f65ad3
Chung-Chiang Cheng <cccheng@synology.com>
2024-09-16 09:30:50 +0800

    chore(nvim): Configure render-markdown plugin
    Refactors the configuration of the render-markdown plugin to use
    setup instead of opts. This allows for a more consistent and
    organized configuration.
    
    Changes:
    - Moves the options for render-markdown from `opts` to `config`.
    - Changes the configuration to use `require("render-markdown").setup`.
    - Removes the unused `dependencies` field.
    
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/markdown.lua
+++ neovim/.config/nvim/lua/plugins/markdown.lua
@@ -5,21 +5,22 @@ return {
         -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
         -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
         dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-        opts = {
-            heading = {
-                sign = false,
-                icons = {},
-            },
-            code = {
-                sign = false,
-                language_pad = 1,
-                left_pad = 1,
-            },
-        },
         keys = {
             { "<LEADER>lm", mode = {"n"}, "<CMD>ToggleMarkdown<CR>", desc = "Markdown render" },
         },
         config = function()
+            require("render-markdown").setup({
+                heading = {
+                    sign = false,
+                    icons = {},
+                },
+                code = {
+                    sign = false,
+                    language_pad = 1,
+                    left_pad = 1,
+                },
+            })
+
             vim.api.nvim_create_user_command("ToggleMarkdown", function()
                 if require("render-markdown.state").enabled then
                     require("render-markdown").disable();
