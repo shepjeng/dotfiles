ccc13fe3b3405ca1f9f61274b8718570ab76d88f
Chung-Chiang Cheng <cccheng@synology.com>
2024-10-07 14:01:27 +0800

    feat(nvim): add tundra and kanagawa-paper themes
    Add tundra and kanagawa-paper as themes.
    
    Modify the theme configuration to include the new themes
    and update the tabline highlight group settings.
    
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/theme.lua
+++ neovim/.config/nvim/lua/plugins/theme.lua
@@ -12,4 +12,31 @@ return {
             vim.api.nvim_set_hl(0, "TabLineSel", { fg = "#9a9ca5", bg = "#2b335a", bold = true })
         end
     },
+    {
+        "sam4llis/nvim-tundra",
+        lazy = true,
+        priority = 1000,
+        config = function()
+            require("nvim-tundra").setup({
+                -- transparent_background = true,
+            })
+            vim.cmd.colorscheme("tundra")
+        end
+    },
+    {
+        "sho-87/kanagawa-paper.nvim",
+        lazy = true,
+        priority = 1000,
+        config = function()
+            require("kanagawa-paper").setup({
+                dimInactive = false,
+                -- transparent = true,
+            })
+            vim.cmd.colorscheme("kanagawa-paper")
+
+            vim.api.nvim_set_hl(0, "TabLine", { fg = "#9a9ca5", bg = "#1f2233", bold = false })
+            vim.api.nvim_set_hl(0, "TabLineFill", { fg = "#101218", bg = "#1f2233", bold = false })
+            vim.api.nvim_set_hl(0, "TabLineSel", { fg = "#9a9ca5", bg = "#2b335a", bold = true })
+        end
+    },
 }
