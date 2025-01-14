ccc7bdfa284a0b44ae9a4e574d5c458506d7c2d1
Chung-Chiang Cheng <cccheng@synology.com>
2025-01-14 16:07:21 +0800

    fix(neovim): Change terminal plugin open mapping
    Change the keybinding to open the terminal from `<LEADER>`` to
    `<LEADER><ESC>` for 65% layout keyboard.
    
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- neovim/.config/nvim/lua/plugins/terminal.lua
+++ neovim/.config/nvim/lua/plugins/terminal.lua
@@ -5,7 +5,8 @@ return {
         config = function()
             require("toggleterm").setup({
                 size = 17,
-                open_mapping = "<LEADER>`",
+                -- open_mapping = "<LEADER>`",
+                open_mapping = "<LEADER><ESC>",
             })
         end
     }
