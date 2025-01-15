ccc8a878b510ff067e86022c0957ef3a5e290616
Chung-Chiang Cheng <cccheng@synology.com>
2025-01-15 10:03:31 +0800

    fix(dev): Improve kernel debugging and remote SSH access
    - Added network forwarding to QEMU for SSH access on port 10022.
    - Changed QEMU's network configuration to use user networking.
    - Updated SSH config to connect to QEMU on localhost port 10022.
    - Added a pidfile for QEMU to improve process management.
    - Removed the old virtio-net device from the QEMU command.
    - Added comments to the QEMU command to improve readability.
    
    Signed-off-by: Chung-Chiang Cheng <cccheng@synology.com>

--- bash/.config/bash/bashrc.d/80-kernel-dev.sh
+++ bash/.config/bash/bashrc.d/80-kernel-dev.sh
@@ -15,16 +15,21 @@ boot-image()
         -cpu host -smp cpus=4,sockets=1,dies=1,cores=4,threads=1 \
         -machine type=q35,usb=off,dump-guest-core=off,hpet=off,accel=kvm \
         -serial mon:stdio \
-        -device virtio-net,netdev=vmnic -netdev bridge,br=virbr0,id=vmnic \
         -device virtio-balloon-pci,id=balloon0,bus=pcie.0,addr=0x4 \
         -device virtio-rng-pci,rng=objrng0,id=rng0,bus=pcie.0,addr=0x1c \
+        -net user,hostfwd=tcp:127.0.0.1:10022-:22 \
+        -net nic,model=virtio-net-pci \
         -object rng-random,id=objrng0,filename=/dev/urandom \
         -append "console=ttyS0 root=/dev/vda earlyprintk=serial net.ifnames=0 nokaslr" \
         -gdb tcp::1234 \
+        -pidfile "$XDG_RUNTIME_DIR/qemu.pid" \
         -kernel "$IMG_KERNEL" \
         -drive if=virtio,format=raw,aio=io_uring,cache=writeback,discard=on,detect-zeroes=on,file="$IMG_ROOT" \
         -drive if=virtio,format=raw,aio=io_uring,cache=writeback,discard=on,detect-zeroes=on,file="$IMG_DISK"
+
+        # -device virtio-net,netdev=vmnic -netdev bridge,br=virbr0,id=vmnic \
 }
+
 
 kgdb()
 {
